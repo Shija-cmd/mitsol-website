7037181f4cd53a5cbc77dce1759ed2d328a93299
Shija-cmd <shijajuma@protonmail.com>
2026-05-26 21:16:55 +0300

    Editing home page

--- config/settings.py
+++ config/settings.py
@@ -28,6 +28,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'services',
+    'portfolio',
+    'contact',
 ]
 
 MIDDLEWARE = [
@@ -46,7 +50,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
