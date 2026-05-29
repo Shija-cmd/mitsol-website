1df482e770edca5fbbc74096be2d666906f1ef17
Shija-cmd <shijajuma@protonmail.com>
2026-05-29 13:26:02 +0300

    Fix production media routing

--- config/urls.py
+++ config/urls.py
@@ -82,7 +82,16 @@ urlpatterns = [
 
 ]
 
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
+if settings.DEBUG:
+
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
+
+else:
+
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
